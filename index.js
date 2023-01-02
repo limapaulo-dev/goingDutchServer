4be5500b4f908abc9ed955c22228e7dfd2b822b4
Paulo Lima <limapaulo.dev@gmail.com>
2023-01-02 15:51:37 -0300

    callback working

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const passport = require('passport');
+const keys = require('./config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => console.log(accessToken)
+  )
+);
+
+
+//route handler '/' none - test
+app.get('/', (req, res) => {
+    res.send({hi:'there'})
+});
+
+//route handler google auth
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+//route handler google auth callback - passport validades the information returned by google
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // dynamic port
 const PORT = process.env.PORT || 5000;
