65d2adabe404a149e03ca0c18961ecad876a1cf4
Paulo Lima <limapaulo.dev@gmail.com>
2023-01-09 22:09:29 -0300

    fixed typo in keys module

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 //keys handler
 if (process.env.NODE_ENV === 'production'){
   //returns prod keys
-  module.exports = require('/prod');
+  module.exports = require('./prod');
 } else {
   //returns dev keys
-  module.exports = require('/dev');
+  module.exports = require('./dev');
 }
 
